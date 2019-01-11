b5dd8f6318af0b4a4381aeac10e1cee5b5c4c7dc
jumpstreet13 <skat131998@mail.ru>
2019-01-12 02:12:57 +0300

    blinking repeat bug is fixed

--- app/src/main/java/com/example/abakarmagomedov/gachisoundpad/GachiAdapter.kt
+++ app/src/main/java/com/example/abakarmagomedov/gachisoundpad/GachiAdapter.kt
@@ -1,56 +1,73 @@
 package com.example.abakarmagomedov.gachisoundpad
 
+import android.animation.ArgbEvaluator
+import android.animation.ObjectAnimator
+import android.animation.ValueAnimator
+import android.graphics.Color
 import android.widget.TextView
 import com.chad.library.adapter.base.BaseQuickAdapter
 import com.chad.library.adapter.base.BaseViewHolder
-import android.graphics.drawable.GradientDrawable
 import android.media.MediaPlayer
+import android.view.animation.Animation
 import android.widget.ImageView
 import androidx.cardview.widget.CardView
 
 
-class GachiAdapter(layout: Int): BaseQuickAdapter<GachiEntity, BaseViewHolder>(layout) {
+class GachiAdapter(layout: Int) : BaseQuickAdapter<GachiEntity, BaseViewHolder>(layout) {
+
+    private val animatorsMap = mutableMapOf<ImageView, ObjectAnimator>()
 
 
     override fun convert(helper: BaseViewHolder?, item: GachiEntity) {
         helper?.run {
-
             getView<TextView>(R.id.gachiSongName)?.text = item.title
 
-            val gd = GradientDrawable(
-                GradientDrawable.Orientation.TOP_BOTTOM,
-                intArrayOf(randInt(0, 999999), randInt(0, 999999))
-            )
-            gd.cornerRadius = 0f
-
             val songContainer = getView<ImageView>(R.id.cardBackground)
-            songContainer.background = gd
+            var animator = animatorsMap[songContainer]
+            if (animator == null) {
+                animator = ObjectAnimator.ofInt(
+                    songContainer, "backgroundColor", Color.WHITE, Color.RED,
+                    Color.WHITE
+                )
+                animator.duration = 1500
+                animator.setEvaluator(ArgbEvaluator())
+                animator.repeatMode = ValueAnimator.REVERSE
+                animator.repeatCount = ValueAnimator.INFINITE
+                animatorsMap[songContainer] = animator
+            }
 
-            val cardView = getView<CardView>(R.id.songContainer)
+            if (!item.isOnRepeat) {
+                animator?.pause()
+                songContainer.background = item.getGradientDrawable()
+            } else {
+                animator?.start()
+            }
 
-            cardView.setOnClickListener {
-                if(!item.isOnRepeat) {
+            songContainer.setOnClickListener {
+                if (!item.isOnRepeat) {
                     val mediaPlayer: MediaPlayer? = item.getMediaPlayer(mContext)
                     mediaPlayer?.seekTo(0)
                     mediaPlayer?.start()
                 }
             }
 
-            cardView.setOnLongClickListener {
+            songContainer.setOnLongClickListener {
                 val mediaPlayer: MediaPlayer? = item.getMediaPlayer(mContext)
-                if(item.isOnRepeat){
+                if (item.isOnRepeat) {
                     mediaPlayer?.isLooping = false
                     item.isOnRepeat = false
                     mediaPlayer?.seekTo(0)
                     mediaPlayer?.pause()
-                }else{
+                    animator?.pause()
+                    songContainer.background = item.getGradientDrawable()
+                } else {
                     mediaPlayer?.run {
                         isLooping = true
                         item.isOnRepeat = true
-                        if(!isPlaying){
+                        if (!isPlaying) {
                             start()
                         }
-                        songContainer.makeBlinkEffect()
+                        animator?.start()
                     }
                 }
                 true
--- app/src/main/java/com/example/abakarmagomedov/gachisoundpad/GachiEntity.kt
+++ app/src/main/java/com/example/abakarmagomedov/gachisoundpad/GachiEntity.kt
@@ -1,12 +1,31 @@
 package com.example.abakarmagomedov.gachisoundpad
 
+import android.animation.ArgbEvaluator
+import android.animation.ObjectAnimator
+import android.animation.ValueAnimator
 import android.content.Context
+import android.graphics.Color
+import android.graphics.drawable.GradientDrawable
 import android.media.MediaPlayer
+import android.view.View
+import android.view.animation.Animation
 import java.lang.IllegalStateException
 
-data class GachiEntity(val title: String, val rawSound: Int, var isOnRepeat: Boolean){
+data class GachiEntity(val title: String, val rawSound: Int, var isOnRepeat: Boolean) {
 
     private var mediaPlayer: MediaPlayer? = null
+    private var gradientDrawable: GradientDrawable? = null
+
+    fun getGradientDrawable(): GradientDrawable? {
+        if (gradientDrawable == null) {
+            gradientDrawable = GradientDrawable(
+                GradientDrawable.Orientation.TOP_BOTTOM,
+                intArrayOf(randInt(0, 999999), randInt(0, 999999))
+            )
+            gradientDrawable?.cornerRadius = 0f
+        }
+        return gradientDrawable
+    }
 
     fun getMediaPlayer(context: Context): MediaPlayer? {
         if (mediaPlayer == null) {
